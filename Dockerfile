60d5f2eb53b798ade0e91d6718f315597cc114d2
EliaSchiavon <e.schiavon1@campus.unimib.it>
2024-06-03 15:14:05 +0200

    Adapting to EUCAIM project requirements

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,31 @@
+## Base image:
 FROM demartis/matlab-runtime:R2023a
 
+LABEL name="image_batch_Trace4Harmonization"
+LABEL version="1.0"
+LABEL authorization="This Dockerfile is intended to build a container image that will be publicly accessible in the CHAIMELEON images repository."
+
+
+############## Things done by the root user ##############
+USER root
+# Installation of tools and requirements:
 RUN apt-get -q update && apt-get install -y -q --no-install-recommends python3 python3-boto3 && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# create the user (and group) "chaimeleon"
+RUN groupadd -g 1000 chaimeleon && \
+    useradd --create-home --shell /bin/bash --uid 1000 --gid 1000 chaimeleon 
+# Default password "chaimeleon" for chaimeleon user. 
+RUN echo "chaimeleon:chaimeleon" | chpasswd
+
+############### Now change to normal user ################
+USER chaimeleon:chaimeleon
+
+# create the directories where some volumes will be mounted
+RUN mkdir -p /home/chaimeleon/datasets && \
+    mkdir -p /home/chaimeleon/persistent-home && \
+    mkdir -p /home/chaimeleon/persistent-shared-folder
+    
+# Copy of the application files into the container:
 COPY ./app /app
+
+WORKDIR /home/chaimeleon
--- app/startup.py
+++ app/startup.py
@@ -1,7 +1,7 @@
 import time
 from shutil import copyfile, unpack_archive
 from datetime import datetime
-import os, boto3
+import os
 import subprocess
 
 #python3
@@ -29,62 +29,30 @@ def run_apply(csv_fpath, parameters_t4r_fpath):
 
 
 
-remote_source_folderpath = os.environ['STORAGE_SOURCE_FOLDERPATH']
-remote_dest_folderpath = os.environ['STORAGE_RESULTS_FOLDERPATH']
 operation = os.environ['OPERATION']
+local_files_folderpath = '/app/files'
 
-local_storage_folderpath = '/app/files'
+
 
 if operation == 'calibrate':
     
     source_calibration_fname = os.environ['TRAINING_CALIBRATION_FILENAME']
-    local_calibration_fpath = os.path.join(local_storage_folderpath, source_calibration_fname)
-    s3 = boto3.client('s3')
-    with open(local_calibration_fpath, 'wb') as f:
-        s3.download_fileobj(remote_source_folderpath, source_calibration_fname, f)
+    local_calibration_fpath = os.path.join(local_files_folderpath, source_calibration_fname)
     run_calibrate(local_calibration_fpath)
-    
-    # TO DO: aggiungere _calibration a var names e.g. harmonized_calibration_results vd. stessa parte in apply elif
-    harmonized_results_fname = f'harmonized_{source_calibration_fname}'
-    harmonized_results_fpath = os.path.join(local_storage_folderpath, harmonized_results_fname)
+    harmonized_calibration_results_fname = f'harmonized_{source_calibration_fname}'
     parameters_fname = 'params.t4r'
-    parameters_fpath = os.path.join(local_storage_folderpath, parameters_fname)
- 
-    remote_results_foldername = f'harmonization_{datetime.now().strftime("%m%d%Y_%H%M%S")}'
-    
-    remote_results_fname = os.path.join(remote_results_foldername, harmonized_results_fname)
-    with open(harmonized_results_fpath, "rb") as f:
-        s3.upload_fileobj(f, remote_dest_folderpath, remote_results_fname)
-
-    remote_params_fname = os.path.join(remote_results_foldername, parameters_fname)
-    with open(parameters_fpath, "rb") as f:
-        s3.upload_fileobj(f, remote_dest_folderpath, remote_params_fname)
 
 
 elif operation == 'apply':
 
     source_application_fname = os.environ['CLASSIFICATION_APPLY_FILENAME']
-    remote_results_foldername = os.environ['REMOTE_RESULTS_FOLDERNAME'] 
 
-    local_application_fpath = os.path.join(local_storage_folderpath, source_application_fname)
-    s3 = boto3.client('s3')
-    with open(local_application_fpath, 'wb') as f:
-        s3.download_fileobj(remote_source_folderpath, source_application_fname, f)
+    local_application_fpath = os.path.join(local_files_folderpath, source_application_fname)
 
     remote_params_fname = 'params.t4r'
-    remote_params_fpath = os.path.join(remote_results_foldername, remote_params_fname)
-    local_params_fpath = os.path.join(local_storage_folderpath, remote_params_fname)
-    s3 = boto3.client('s3')
-    with open(local_params_fpath, 'wb') as f:
-        s3.download_fileobj(remote_dest_folderpath, remote_params_fpath, f)
+    local_params_fpath = os.path.join(local_files_folderpath, remote_params_fname)
 
     run_apply(local_application_fpath, local_params_fpath)
-
-    local_harmonized_apply_results_fname = f'harmonized_{source_application_fname}'
-    local_harmonized_apply_results_fpath = os.path.join(local_storage_folderpath, local_harmonized_apply_results_fname)
-    remote_harmonized_results_fname = os.path.join(remote_results_foldername, local_harmonized_apply_results_fname)
-    with open(local_harmonized_apply_results_fpath, "rb") as f:
-        s3.upload_fileobj(f, remote_dest_folderpath, remote_harmonized_results_fname)
 
 
 else:
